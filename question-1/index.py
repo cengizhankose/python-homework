139eaa4b468263fdf6496b5f97a4cd6327bed985
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-04-30 14:20:43 +0300

    first question

---
+++ question-1/index.py
@@ -0,0 +1,9 @@
+print("Enter first number: ")
+num1 = input()
+print("Enter first number: ")
+num2 = input()
+
+if(num1 < num2):
+    print("Greatest number is "+num2)
+else:
+    print("Greatest number is "+num1)
